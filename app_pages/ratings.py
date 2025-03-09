0586ebda6e179473df3edd53b6d663d0e7257674
Amlaith <burjooi@gmail.com>
2025-03-10 01:44:20 +0300

    center rating

--- app_pages/ratings.py
+++ app_pages/ratings.py
@@ -22,16 +22,17 @@ show_pros = st.checkbox('показать нонеймов')
 players_df = pub_stats.join(players.set_index('playerId'), on='playerId')
 players_df['totalRoundsPlayed'] = players_df['roundsPlayed_normal'] + players_df['roundsPlayed_ranked']
 players_df = players_df.replace({'totalRoundsPlayed': {0: 999}})
-players_df['rating'] = (players_df['damageDealt_normal'] * players_df['roundsPlayed_normal'] + players_df['damageDealt_ranked'] * players_df['roundsPlayed_ranked'] * ranked_coef) / (players_df['totalRoundsPlayed']) * (adr_coef / (adr_coef + 1)) + players_df['level'] * (1 / (adr_coef + 1))
+# players_df['rating'] = (players_df['damageDealt_normal'] * players_df['roundsPlayed_normal'] + players_df['damageDealt_ranked'] * players_df['roundsPlayed_ranked'] * ranked_coef) / (players_df['totalRoundsPlayed']) * (adr_coef / (adr_coef + 1)) + players_df['level'] * (1 / (adr_coef + 1))
+players_df['rating'] = (players_df['damageDealt_normal'] * players_df['roundsPlayed_normal'] + players_df['damageDealt_ranked'] * players_df['roundsPlayed_ranked'] * ranked_coef) / (players_df['totalRoundsPlayed']) * (5.43) + players_df['level'] * (0.32)
 players_df = players_results.join(players_df.set_index(['playerName']), on='playerName', how='right').reset_index()
 players_df = players_df.join(teams.set_index(['tournamentId', 'teamId']), on=['tournamentId', 'teamId'])
 players_df_unique_teams = players_df.drop(['tournamentId', 'teamId'], axis=1).drop_duplicates()
 # players_df_unique_teams = players_df
-
+# st.dataframe(players_df_unique_teams)
 players_column_config = {
     'playerName': 'Игровой ник', 
     'teamName': 'team',
-    'rating': 'rating', 
+    'rating': st.column_config.NumberColumn('rating', format='%.0f'), 
     'damageDealt_normal': 'ADR normal',
     'damageDealt_ranked': 'ADR ranked',
     'level': 'lvl',
@@ -60,12 +61,18 @@ players_results = players_results.join(players_df[['playerId', 'playerName', 'ra
 players_results = players_results.groupby(['tournamentId', 'teamId']).agg({'rating': lambda x: (sum(x ** 2)) ** (1/2) })
 players_results = players_results.join(teams.set_index(['tournamentId', 'teamId']), on=['tournamentId', 'teamId']).reset_index()
 
+
+
+teams_column_config = {
+    'rating': st.column_config.NumberColumn('rating', format='%.0f'), 
+}
+
 teams_column_order = (
     'tournamentId',
     'teamName',
     'rating',
 )
 
-st.dataframe(players_results, column_order=teams_column_order)
+st.dataframe(players_results, column_order=teams_column_order, column_config=teams_column_config)
 
 
